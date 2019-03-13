e8f0412b22057e6552ddc4604bba47f11937da8a
ShaneBeee shanebolenback@me.com <shanebolenback@me.com>
2019-03-13 01:59:51 -0700

    ChatToggle - update - Added tab complete - Reformatted how the command is loaded on startup - Fixed messages

--- src/com/fattymieo/survival/commands/ToggleChat.java
+++ src/com/fattymieo/survival/commands/ToggleChat.java
@@ -4,45 +4,56 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Objective;
 
 import com.fattymieo.survival.Survival;
+import org.bukkit.util.StringUtil;
 
-public class ToggleChat implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToggleChat implements CommandExecutor, TabCompleter {
 
 	private Objective chat = Survival.board.getObjective("Chat");
 
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		String prefix = "&7[&3SurvivalPlus&7] ";
 		if (command.getName().equalsIgnoreCase("togglechat")) {
 			if (!(sender instanceof Player)) {
 				sender.sendMessage(Survival.Words.get("Works on players only"));
-				return false;
+				return true;
+			}
+			Player player = (Player) sender;
+			//if (LocalChatDist > -1)
+			if (Survival.LocalChatDist <= -1) {
+				player.sendMessage(Survival.Words.get("Toggled Chat Disabled"));
+				return true;
 			}
 
 			if (args.length == 1) {
-				Player player = (Player) sender;
+
 				switch (args[0]) {
 					case "local":
 					case "l":
-						player.sendMessage(Survival.Words.get("�bToggled to �aLocal�b Chat"));
+						player.sendMessage(Survival.Words.get("Toggled to Local Chat"));
 						chat.getScore(player.getName()).setScore(0);
 						break;
 					case "global":
 					case "g":
-						player.sendMessage(Survival.Words.get("�bToggled to �2Global�b Chat"));
+						player.sendMessage(Survival.Words.get("Toggled to Global Chat"));
 						chat.getScore(player.getName()).setScore(1);
 						break;
 					default:
 						return false;
 				}
 			} else if (args.length == 0) {
-				Player player = (Player) sender;
 				if (chat.getScore(player.getName()).getScore() == 0) {
-					player.sendMessage(Survival.Words.get("�bToggled to �2Global�b Chat"));
+					player.sendMessage(Survival.Words.get("Toggled to Global Chat"));
 					chat.getScore(player.getName()).setScore(1);
 				} else {
-					player.sendMessage(Survival.Words.get("�bToggled to �aLocal�b Chat"));
+					player.sendMessage(Survival.Words.get("Toggled to Local Chat"));
 					chat.getScore(player.getName()).setScore(0);
 				}
 			} else {
@@ -51,7 +62,28 @@ public class ToggleChat implements CommandExecutor {
 			}
 
 			return true;
-		} else return false;
+		} else {
+			sender.sendMessage("Command: " + command.getName());
+			return true;
+		}
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String s, String[] args) {
+		StringBuilder builder = new StringBuilder();
+		for (String arg : args) {
+			builder.append(arg).append(" ");
+		}
+		String[] list = {"local", "global"};
+		String[] list2 = {""};
+		String arg = builder.toString().trim();
+		ArrayList<String> matches = new ArrayList<>();
+		for (String name : (Survival.LocalChatDist > -1) ? list : list2) {
+			if (StringUtil.startsWithIgnoreCase(name, arg)) {
+				matches.add(name);
+			}
+		}
+		return matches;
 	}
 
 }
