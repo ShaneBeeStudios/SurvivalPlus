585536491108858d86483f5b3c3c7423a736255c
ShaneBeee <shanebolenback@me.com>
2019-04-27 21:56:44 -0700

    CompassWaypoint - Changes - Changed up some of the item checks using tags and utils

--- src/tk/shanebee/survival/events/CompassWaypoint.java
+++ src/tk/shanebee/survival/events/CompassWaypoint.java
@@ -23,26 +23,6 @@ public class CompassWaypoint implements Listener {
 				if (mainItem.getType() == Material.COMPASS || offItem.getType() == Material.COMPASS) {
 					if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 						switch (event.getClickedBlock().getType()) {
-							case ENCHANTING_TABLE:
-							case ANVIL:
-							case BREWING_STAND:
-							case SPRUCE_DOOR:
-							case BIRCH_DOOR:
-							case OAK_DOOR:
-							case JUNGLE_DOOR:
-							case ACACIA_DOOR:
-							case DARK_OAK_DOOR:
-							case IRON_DOOR:
-							case TRAPPED_CHEST:
-							case CHEST:
-							case NOTE_BLOCK:
-							case SPRUCE_FENCE_GATE:
-							case BIRCH_FENCE_GATE:
-							case JUNGLE_FENCE_GATE:
-							case DARK_OAK_FENCE_GATE:
-							case ACACIA_FENCE_GATE:
-							case IRON_TRAPDOOR:
-							case FURNACE:
 							case HOPPER:
 							case CRAFTING_TABLE:
 							case DROPPER:
@@ -52,7 +32,10 @@ public class CompassWaypoint implements Listener {
 						}
 						if (Tag.BEDS.isTagged(event.getClickedBlock().getType())) return;
 						if (Utils.isWoodGate(event.getClickedBlock().getType())) return;
-						if (Tag.TRAPDOORS.isTagged(event.getClickedBlock().getType())) return;
+						if (Tag.DOORS.isTagged(event.getClickedBlock().getType())) return;
+						if (Utils.isCookingBlock(event.getClickedBlock().getType())) return;
+						if (Utils.isStorageBlock(event.getClickedBlock().getType())) return;
+						if (Utils.isUtilityBlock(event.getClickedBlock().getType())) return;
 
 						Location loc = event.getClickedBlock().getRelative(event.getBlockFace()).getLocation();
 						player.sendMessage(ChatColor.LIGHT_PURPLE + Utils.getColoredString(Survival.lang.compass_pointed) + " (" + loc.getX() + ", " + loc.getY() + ", " + loc.getZ() + ").");
