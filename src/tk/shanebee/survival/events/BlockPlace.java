d3c3039904684e3f4e2bbf8b3fc215a43dcd1d26
ShaneBeee <shanebolenback@me.com>
2019-04-27 20:46:46 -0700

    BlockPlace - Changes - Updated with 1.14 blocks - Updated with tool specifics - Removed some unnecessary util tags

--- src/tk/shanebee/survival/events/BlockPlace.java
+++ src/tk/shanebee/survival/events/BlockPlace.java
@@ -1,7 +1,5 @@
 package tk.shanebee.survival.events;
 
-import tk.shanebee.survival.Survival;
-import tk.shanebee.survival.util.Utils;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -11,6 +9,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
+import tk.shanebee.survival.Survival;
+import tk.shanebee.survival.managers.Items;
+import tk.shanebee.survival.util.Utils;
 
 import java.util.Random;
 
@@ -29,25 +30,25 @@ public class BlockPlace implements Listener {
 		if (player.getGameMode() == GameMode.SURVIVAL || player.getGameMode() == GameMode.ADVENTURE) {
 			if (Survival.settings.getBoolean("Survival.PlaceOnlyWith.Hammer")) {
 				if (checkArtifact(block)) {
-					if (offTool.getType() == Material.WOODEN_SWORD) {
+					if (Items.compare(offTool, Items.HAMMER)) {
 						Random rand = new Random();
 						int chance_reduceDur = rand.nextInt(10) + 1;
 						if (chance_reduceDur == 1) {
 							Utils.setDurability(offTool, Utils.getDurability(offTool) + 1);
 						}
 
-						if (Utils.getDurability(offTool) >= 59) {
+						if (Utils.getDurability(offTool) >= offTool.getType().getMaxDurability()) {
 							player.getLocation().getWorld().playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0F, rand.nextFloat() * 0.4F + 0.8F);
 							player.getInventory().setItemInOffHand(null);
 						}
-					} else if (mainTool.getType() == Material.WOODEN_SWORD) {
+					} else if (Items.compare(mainTool, Items.HAMMER)) {
 						Random rand = new Random();
 						int chance_reduceDur = rand.nextInt(10) + 1;
 						if (chance_reduceDur == 1) {
 							Utils.setDurability(mainTool, ((Damageable) mainTool.getItemMeta()).getDamage() + 1);
 						}
 
-						if (Utils.getDurability(mainTool) >= 59) {
+						if (Utils.getDurability(mainTool) >= mainTool.getType().getMaxDurability()) {
 							player.getLocation().getWorld().playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0F, rand.nextFloat() * 0.4F + 0.8F);
 							player.getInventory().setItemInOffHand(null);
 						}
@@ -65,54 +66,40 @@ public class BlockPlace implements Listener {
 		Material material = block.getType();
 		return Tag.DOORS.isTagged(material)
 
-				|| material == Material.CHEST
-				|| material == Material.TRAPPED_CHEST
-
-
-				|| Utils.isWoodFence(material)
 				|| Utils.isWoodGate(material)
-				|| Utils.isBed(material)
 				|| Utils.isTerracotta(material)
 				|| Utils.isGlazedTerracotta(material)
-				|| Utils.isWall(material)
 				|| Utils.isConcrete(material)
 				|| Utils.isConcretePowder(material)
+				|| Utils.isStoneTypeBlock(material)
+				|| Utils.isCookingBlock(material)
+				|| Utils.isStorageBlock(material)
+				|| Utils.isUtilityBlock(material)
+				|| Utils.isShulkerBox(material)
+				|| Utils.isOreBlock(material)
 
+				|| Tag.BEDS.isTagged(material)
 				|| Tag.LOGS.isTagged(material)
 				|| Tag.STAIRS.isTagged(material)
 				|| Tag.SLABS.isTagged(material)
 				|| Tag.PLANKS.isTagged(material)
 				|| Tag.WOODEN_PRESSURE_PLATES.isTagged(material)
+				|| Tag.WOODEN_FENCES.isTagged(material)
 				|| Tag.RAILS.isTagged(material)
-
-				|| material == Material.JUKEBOX
-				|| material == Material.BOOKSHELF
-				|| material == Material.NOTE_BLOCK
-				|| material == Material.LADDER
 				|| Tag.SIGNS.isTagged(material)
 				|| Tag.BANNERS.isTagged(material)
-				|| material == Material.IRON_TRAPDOOR
-				|| material == Material.BRICK
-				|| material == Material.NETHER_BRICK
-				|| material == Material.NETHER_BRICK_FENCE
-				|| material == Material.STONE_BRICKS
-				|| material == Material.PRISMARINE
-				|| material == Material.SANDSTONE
-				|| material == Material.RED_SANDSTONE
+				|| Tag.FENCES.isTagged(material)
+				|| Tag.FENCES.isTagged(material)
+				|| Tag.SIGNS.isTagged(material)
+
+				|| material == Material.BOOKSHELF
+				|| material == Material.LADDER
 				|| material == Material.SEA_LANTERN
 				|| material == Material.GLOWSTONE
-				|| material == Material.PURPUR_BLOCK
-				|| material == Material.PURPUR_PILLAR
 				|| material == Material.END_ROD
 				|| material == Material.DISPENSER
 				|| material == Material.DROPPER
-				|| material == Material.FURNACE
-				|| material == Material.ENCHANTING_TABLE
-				|| material == Material.ANVIL
-				|| material == Material.ENDER_CHEST
 				|| material == Material.HOPPER
-				|| material == Material.CAULDRON
-				|| material == Material.BREWING_STAND
 				|| material == Material.STONE_PRESSURE_PLATE
 				|| material == Material.LIGHT_WEIGHTED_PRESSURE_PLATE
 				|| material == Material.HEAVY_WEIGHTED_PRESSURE_PLATE
