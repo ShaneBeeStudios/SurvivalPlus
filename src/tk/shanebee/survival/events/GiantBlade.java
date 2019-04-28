1729ef55a3271891d6f7a7291a6b03bfe218c7e8
ShaneBeee <shanebolenback@me.com>
2019-04-28 02:16:55 -0700

    GiantBlade - Changes - Using Items enums instead of material types

--- src/tk/shanebee/survival/events/GiantBlade.java
+++ src/tk/shanebee/survival/events/GiantBlade.java
@@ -3,6 +3,7 @@ package tk.shanebee.survival.events;
 import java.util.Collection;
 import java.util.Random;
 
+import tk.shanebee.survival.managers.Items;
 import tk.shanebee.survival.util.Utils;
 import org.bukkit.*;
 import org.bukkit.entity.Entity;
@@ -43,7 +44,7 @@ public class GiantBlade implements Listener {
 
 			Random rand = new Random();
 
-			if (offItem.getType() == Material.GOLDEN_HOE) {
+			if (Items.compare(offItem, Items.ENDER_GIANT_BLADE)) {
 				if (event.getDamager() instanceof LivingEntity && event.getCause() == DamageCause.ENTITY_ATTACK) {
 					LivingEntity enemy = (LivingEntity) event.getDamager();
 					enemy.damage(event.getDamage() * 40 / 100, player);
@@ -73,7 +74,7 @@ public class GiantBlade implements Listener {
 
 		Score score_dualWieldMsg = tech_dualWieldMsg.getScore(player.getName());
 
-		if (mainItem.getType() == Material.GOLDEN_HOE) {
+		if (Items.compare(mainItem, Items.ENDER_GIANT_BLADE)) {
 			if (dualWield.getScore(player.getName()).getScore() == 0) {
 				if (event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.RIGHT_CLICK_AIR) {
 					if (player.isSprinting()) {
