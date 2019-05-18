915e670619d6e53922b6fa4f17709bda808f3811
ShaneBeee <shanebolenback@me.com>
2019-05-17 22:57:46 -0700

    SnowballThrow - Fixes - Fixed snow layer breaking throwing an error - Fixed snow layers not building up when throwing a snowball

--- src/tk/shanebee/survival/events/SnowballThrow.java
+++ src/tk/shanebee/survival/events/SnowballThrow.java
@@ -20,35 +20,39 @@ import org.bukkit.util.BlockIterator;
 public class SnowballThrow implements Listener
 {
 	@EventHandler
-	public void onThrowingSnowball(ProjectileHitEvent e)
-	{
-		if(e.getEntity() instanceof Snowball)
-		{
-			Snowball snowball = (Snowball)e.getEntity();
-			
-			BlockIterator iterator = new BlockIterator(snowball.getWorld(), snowball.getLocation().toVector(), snowball.getVelocity().normalize(), 0.0D, 4);
+	public void onThrowingSnowball(ProjectileHitEvent e) {
+		if (e.getEntity() instanceof Snowball) {
+			Snowball snowball = (Snowball) e.getEntity();
+
+			BlockIterator iterator = new BlockIterator(snowball.getWorld(), snowball.getLocation().toVector(),
+					snowball.getVelocity().normalize(), 0.0D, 4);
 			Block actual = null;
-			while (iterator.hasNext())
-			{
+			while (iterator.hasNext()) {
 				actual = iterator.next();
-				 
+
 				if (actual.getState().getType() != Material.AIR)
 					break;
 			}
 
 			assert actual != null;
-			switch(actual.getType())
-			{
+			switch (actual.getType()) {
 				case SNOW:
+					Snow snow = ((Snow) actual.getBlockData());
+					if (snow.getLayers() == 7)
+						actual.setType(Material.SNOW_BLOCK);
+					else {
+						snow.setLayers(snow.getLayers() + 1);
+						actual.setBlockData(snow);
+					}
+					break;
 				case TALL_GRASS:
 				case DANDELION:
 				case ROSE_BUSH:
 				case DEAD_BUSH:
-			 		actual.setType(Material.SNOW);
+					actual.setType(Material.SNOW);
 					break;
 				default:
-					if(actual.getType().isSolid())
-					{
+					if (actual.getType().isSolid()) {
 						Block aboveHit = actual.getRelative(BlockFace.UP);
 						if (aboveHit.getType() == Material.AIR) {
 							aboveHit.setType(Material.SNOW);
@@ -59,15 +63,12 @@ public class SnowballThrow implements Listener
 	}
 	
 	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onBreakSnow(BlockBreakEvent e)
-	{
-		if(e.isCancelled()) return;
+	public void onBreakSnow(BlockBreakEvent e) {
+		if (e.isCancelled()) return;
 		Player player = e.getPlayer();
-		if(player.getGameMode() == GameMode.SURVIVAL || player.getGameMode() == GameMode.ADVENTURE)
-		{
+		if (player.getGameMode() == GameMode.SURVIVAL || player.getGameMode() == GameMode.ADVENTURE) {
 			ItemStack mainItem = player.getInventory().getItemInMainHand();
-			switch(mainItem.getType())
-			{
+			switch (mainItem.getType()) {
 				case WOODEN_SHOVEL:
 				case GOLDEN_SHOVEL:
 				case STONE_SHOVEL:
@@ -76,30 +77,30 @@ public class SnowballThrow implements Listener
 					Block block = e.getBlock();
 					ItemMeta meta = mainItem.getItemMeta();
 					assert meta != null;
-					switch(block.getType())
-					{
+					switch (block.getType()) {
 						case SNOW:
 							e.setCancelled(true);
 
 
 							((Damageable) meta).setDamage(((Damageable) meta).getDamage() + 1);
 							mainItem.setItemMeta(meta);
-							if(((Damageable) meta).getDamage() >= mainItem.getType().getMaxDurability() + 1)
+							if (((Damageable) meta).getDamage() >= mainItem.getType().getMaxDurability() + 1)
 								player.getInventory().setItemInMainHand(null);
-				            player.updateInventory();
-				            block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(Material.SNOWBALL, (((Snow) block.getState()).getLayers()) + 1));
-				            block.setType(Material.AIR);
+							player.updateInventory();
+							block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(Material.SNOWBALL,
+									(((Snow) block.getBlockData()).getLayers()) + 1));
+							block.setType(Material.AIR);
 							break;
 						case SNOW_BLOCK:
 							e.setCancelled(true);
 							((Damageable) meta).setDamage(((Damageable) meta).getDamage() + 1);
 							mainItem.setItemMeta(meta);
-							if(((Damageable) meta).getDamage() >= mainItem.getType().getMaxDurability() + 1)
+							if (((Damageable) meta).getDamage() >= mainItem.getType().getMaxDurability() + 1)
 								player.getInventory().setItemInMainHand(null);
-				            player.updateInventory();
-				            block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(Material.SNOWBALL, 8));
-				            block.setType(Material.AIR);
-				            break;
+							player.updateInventory();
+							block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(Material.SNOWBALL, 8));
+							block.setType(Material.AIR);
+							break;
 						default:
 					}
 					break;
@@ -107,4 +108,5 @@ public class SnowballThrow implements Listener
 			}
 		}
 	}
+
 }
