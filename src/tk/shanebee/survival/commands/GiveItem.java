911de29cbd004ab20e2602f44e92c5beb54a7fd7
ShaneBeee <shanebolenback@me.com>
2019-06-14 21:55:48 -0700

    GiveItem - Changes - Add the item directly to the players inventory rather than drop item - Drop the item if the player's inventory is full - Add an "amount" to the command, allowing to give more than 1 item at once

--- src/tk/shanebee/survival/commands/GiveItem.java
+++ src/tk/shanebee/survival/commands/GiveItem.java
@@ -1,39 +1,51 @@
 package tk.shanebee.survival.commands;
 
-import tk.shanebee.survival.Survival;
-import tk.shanebee.survival.managers.Items;
-import tk.shanebee.survival.util.Utils;
 import com.google.common.collect.ImmutableList;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.StringUtil;
+import tk.shanebee.survival.Survival;
+import tk.shanebee.survival.managers.Items;
+import tk.shanebee.survival.util.Utils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+@SuppressWarnings("NullableProblems")
 public class GiveItem implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         String prefix = Utils.getColoredString(Survival.lang.prefix);
-        if (args.length != 2) return true;
-        OfflinePlayer player = Bukkit.getPlayer(args[0]);
-        Items itemName;
+        if (args.length < 2) return true;
+        Player player = Bukkit.getPlayer(args[0]);
         if (player != null) {
+            Items itemName;
+            int amount = 1;
+            try {
+                if (args.length == 3) {
+                    amount = Integer.valueOf(args[2]);
+                }
+            } catch (IllegalArgumentException ignore) {}
             try {
                 itemName = Items.valueOf(args[1].toUpperCase());
-                Location loc = ((Player) player).getLocation();
+                ItemStack item = Items.get(itemName);
+                item.setAmount(amount);
+
+                Location loc = player.getLocation();
                 loc.setY(loc.getY() + 1);
-                Bukkit.getWorld(((Player) player).getWorld().getName()).dropItem(loc, Items.get(itemName));
 
+                if (player.getInventory().addItem(item).size() != 0) {
+                    player.getWorld().dropItem(loc, item);
+                }
                 if (sender instanceof Player) {
-
                     Utils.sendColoredMsg(sender, prefix + "&6You gave &b" + itemName + " &6to &b" + player.getName());
                 } else {
                     Utils.sendColoredMsg(sender, prefix + "&6CONSOLE gave &b" + itemName + " &6to &b" + player.getName());
@@ -49,17 +61,21 @@ public class GiveItem implements CommandExecutor, TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String s, String[] args) {
-        if (args.length == 0 || args.length >= 3) {
+        if (args.length == 0 || args.length >= 4) {
             return ImmutableList.of();
         }
         if (args.length <= 1) return null;
-        ArrayList<String> matches = new ArrayList<>();
-        for (Items name : Items.values()) {
-            if (StringUtil.startsWithIgnoreCase(name.toString(), args[1])) {
-                matches.add(name.toString());
+        if (args.length == 2) {
+            ArrayList<String> matches = new ArrayList<>();
+            for (Items name : Items.values()) {
+                if (StringUtil.startsWithIgnoreCase(name.toString(), args[1])) {
+                    matches.add(name.toString());
+                }
             }
+            return matches;
+        } else {
+            return Collections.singletonList("<amount>");
         }
-        return matches;
     }
 
 }
