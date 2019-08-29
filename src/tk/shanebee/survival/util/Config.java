28a4e85da8e604cf3ef1eaa96fbd586561927c13
ShaneBeee <shanebolenback@me.com>
2019-08-29 01:54:01 -0700

    Config - changes - Add a few more config fields

--- src/tk/shanebee/survival/util/Config.java
+++ src/tk/shanebee/survival/util/Config.java
@@ -12,6 +12,8 @@ public class Config {
 	private Survival plugin;
 	private FileConfiguration settings;
 
+	public String RESOURCE_PACK_URL;
+
 	public boolean SURVIVAL_ENABLED;
 	public boolean SURVIVAL_TORCH;
 
@@ -27,12 +29,14 @@ public class Config {
 
 	public boolean STATUS_SCOREBOARD;
 
-	public boolean MECHANICS_THIRST_ENABLED;
 	public int THIRST_START_AMOUNT;
 	public int THIRST_RESPAWN_AMOUNT;
 	public boolean THIRST_PURIFY_WATER;
 	public int HUNGER_START_AMOUNT;
 	public int HUNGER_RESPAWN_AMOUNT;
+
+	public boolean MECHANICS_SCOREBOARD;
+
 	public boolean MECHANICS_REDUCED_IRON_NUGGET;
 	public boolean MECHANICS_REDUCED_GOLD_NUGGET;
 	public boolean MECHANICS_SLOW_ARMOR;
@@ -41,6 +45,15 @@ public class Config {
 
 	public boolean MECHANICS_FARMING_PRODUCTS_COOKIE;
 	public boolean MECHANICS_FARMING_PRODUCTS_BREAD;
+
+	public boolean MECHANICS_THIRST_ENABLED;
+	public double MECHANICS_THIRST_DRAIN_RATE;
+
+	public boolean MECHANICS_FOOD_DIVERSITY;
+
+	public boolean MECHANICS_FATIGUE_LEVEL;
+	public int MECHANICS_FATIGUE_INCREASE_CHANCE;
+	public int MECHANICS_FATIGUE_REFRESH_TIME;
 
 	public boolean RECIPES_FISHING_ROD;
 	public boolean RECIPES_GRANITE;
@@ -97,6 +110,8 @@ public class Config {
 	}
 
 	private void loadSettings() {
+
+		this.RESOURCE_PACK_URL = settings.getString("MultiWorld.ResourcePackURL");
 		this.SURVIVAL_ENABLED = settings.getBoolean("Survival.Enabled");
 		this.SURVIVAL_TORCH = settings.getBoolean("Survival.Torch");
 
@@ -112,12 +127,13 @@ public class Config {
 
 		this.STATUS_SCOREBOARD = settings.getBoolean("Mechanics.StatusScoreboard");
 
-		this.MECHANICS_THIRST_ENABLED = settings.getBoolean("Mechanics.Thirst.Enabled");
 		this.THIRST_START_AMOUNT = settings.getInt("Mechanics.Thirst.Starting-Amount");
 		this.THIRST_RESPAWN_AMOUNT = settings.getInt("Mechanics.Thirst.Respawn-Amount");
 		this.THIRST_PURIFY_WATER = settings.getBoolean("Mechanics.Thirst.PurifyWater");
 		this.HUNGER_START_AMOUNT = settings.getInt("Mechanics.Hunger.Starting-Amount");
 		this.HUNGER_RESPAWN_AMOUNT = settings.getInt("Mechanics.Hunger.Respawn-Amount");
+
+		this.MECHANICS_SCOREBOARD = settings.getBoolean("Mechanics.StatusScoreboard");
 		this.MECHANICS_REDUCED_IRON_NUGGET = settings.getBoolean("Mechanics.ReducedIronNugget");
 		this.MECHANICS_REDUCED_GOLD_NUGGET = settings.getBoolean("Mechanics.ReducedGoldNugget");
 		this.MECHANICS_SLOW_ARMOR = settings.getBoolean("Mechanics.SlowArmor");
@@ -126,6 +142,15 @@ public class Config {
 
 		this.MECHANICS_FARMING_PRODUCTS_COOKIE = settings.getBoolean("Mechanics.FarmingProducts.Cookie");
 		this.MECHANICS_FARMING_PRODUCTS_BREAD = settings.getBoolean("Mechanics.FarmingProducts.Bread");
+
+		this.MECHANICS_THIRST_ENABLED = settings.getBoolean("Mechanics.Thirst.Enabled");
+		this.MECHANICS_THIRST_DRAIN_RATE = settings.getDouble("Mechanics.Thirst.DrainRate");
+
+		this.MECHANICS_FOOD_DIVERSITY = settings.getBoolean("Mechanics.FoodDiversity");
+
+		this.MECHANICS_FATIGUE_LEVEL = settings.getBoolean("Mechanics.BedFatigueLevel");
+		this.MECHANICS_FATIGUE_INCREASE_CHANCE = settings.getInt("Mechanics.FatigueLevelIncreaseChance");
+		this.MECHANICS_FATIGUE_REFRESH_TIME = settings.getInt("Mechanics.BedFatigueRefreshTime");
 
 		this.RECIPES_FISHING_ROD = settings.getBoolean("Recipes.FishingRod");
 		this.RECIPES_GRANITE = settings.getBoolean("Recipes.Granite");
