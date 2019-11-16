f6dd9b8830fba7dd41f9ffd5e4b7d6465ce965e2
ShaneBeee <shanebolenback@me.com>
2019-11-16 03:30:17 -0800

    Nutrition - changes - Reorganized enums by groups (will be useful in the NutritionGUI) - Added constructor for use with custom items (future use) - Added javadoc notes

--- src/main/java/tk/shanebee/survival/data/Nutrition.java
+++ src/main/java/tk/shanebee/survival/data/Nutrition.java
@@ -1,54 +1,66 @@
 package tk.shanebee.survival.data;
 
 import org.bukkit.Material;
+import tk.shanebee.survival.managers.Items;
 
+/**
+ * Nutritional values for foods
+ */
 public enum Nutrition {
 
-	PUMPKIN_PIE(300, 50, 60, Material.PUMPKIN_PIE),
-	RABBIT_STEW(200, 225, 240, Material.RABBIT_STEW),
-	BREAD(300, 25, 12, Material.BREAD),
+	// FRUITS AND VEGGIES
 	POTATO(25, 0, 10, Material.POTATO),
 	BAKED_POTATO(200, 0, 60, Material.BAKED_POTATO),
 	POISONOUS_POTATO(50, 0, 8, Material.POISONOUS_POTATO),
 	APPLE(50, 0, 70, Material.APPLE),
 	GOLDEN_APPLE(50, 0, 70, Material.GOLDEN_APPLE),
 	ENCHANTED_GOLDEN_APPLE(50, 0, 70, Material.ENCHANTED_GOLDEN_APPLE),
-	SWEET_BERRIES(40, 0, 60, Material.SWEET_BERRIES),
+	CARROT(0, 0, 105, Material.CARROT),
+	GOLDEN_CARROT(0, 0, 25, Material.GOLDEN_CARROT),
 	CHORUS_FRUIT(25, 0, 35, Material.CHORUS_FRUIT),
 	MELON_SLICE(25, 0, 35, Material.MELON_SLICE),
+	BEETROOT(0, 0, 35, Material.BEETROOT),
+	DRIED_KELP(15, 50, 50, Material.DRIED_KELP),
+	SWEET_BERRIES(40, 0, 60, Material.SWEET_BERRIES),
+
+	// PREPARED FOODS
+	BREAD(300, 25, 12, Material.BREAD),
+	PUMPKIN_PIE(300, 50, 60, Material.PUMPKIN_PIE),
+	RABBIT_STEW(200, 225, 240, Material.RABBIT_STEW),
 	MUSHROOM_STEW(0, 50, 200, Material.MUSHROOM_STEW),
 	SUSPICIOUS_STEW(0, 50, 210, Material.SUSPICIOUS_STEW),
 	BEETROOT_SOUP(0, 50, 200, Material.BEETROOT_SOUP),
 	COOKIE(107, 11, 3, Material.COOKIE),
-	MILK_BUCKET(0, 250, 0, Material.MILK_BUCKET),
-	BEETROOT(0, 0, 35, Material.BEETROOT),
-	CARROT(0, 0, 105, Material.CARROT),
-	GOLDEN_CARROT(0, 0, 25, Material.GOLDEN_CARROT),
-	COOKED_COD(0, 225, 0, Material.COOKED_COD),
-	COOKED_SALMON(0, 225, 0, Material.COOKED_SALMON),
-	COOKED_BEEF(0, 200, 0, Material.COOKED_BEEF),
-	COOKED_CHICKEN(0, 200, 0, Material.COOKED_CHICKEN),
-	COOKED_MUTTON(0, 200, 0, Material.COOKED_MUTTON),
-	COOKED_PORKCHOP(0, 200, 0, Material.COOKED_PORKCHOP),
-	COOKED_RABBIT(0, 200, 0, Material.COOKED_RABBIT),
-	SALMON(0, 75, 0, Material.SALMON),
-	COD(0, 75, 0, Material.COD),
-	PUFFERFISH(0, 75, 0, Material.PUFFERFISH),
-	TROPICAL_FISH(0, 75, 0, Material.TROPICAL_FISH),
+	CAKE(171, 114, 3, Material.CAKE),
+
+	// MEATS
 	BEEF(0, 50, 0, Material.BEEF),
 	CHICKEN(0, 50, 0, Material.CHICKEN),
 	MUTTON(0, 50, 0, Material.MUTTON),
 	PORKCHOP(0, 50, 0, Material.PORKCHOP),
 	RABBIT(0, 50, 0, Material.RABBIT),
+	COOKED_BEEF(0, 200, 0, Material.COOKED_BEEF),
+	COOKED_CHICKEN(0, 200, 0, Material.COOKED_CHICKEN),
+	COOKED_MUTTON(0, 200, 0, Material.COOKED_MUTTON),
+	COOKED_PORKCHOP(0, 200, 0, Material.COOKED_PORKCHOP),
+	COOKED_RABBIT(0, 200, 0, Material.COOKED_RABBIT),
+	COD(0, 75, 0, Material.COD),
+	SALMON(0, 75, 0, Material.SALMON),
+	PUFFERFISH(0, 75, 0, Material.PUFFERFISH),
+	TROPICAL_FISH(0, 75, 0, Material.TROPICAL_FISH),
+	COOKED_COD(0, 225, 0, Material.COOKED_COD),
+	COOKED_SALMON(0, 225, 0, Material.COOKED_SALMON),
+
+	// MISC
 	SPIDER_EYE(0, 50, 0, Material.SPIDER_EYE),
 	ROTTEN_FLESH(0, 25, 25, Material.ROTTEN_FLESH),
-	DRIED_KELP(15, 50, 50, Material.DRIED_KELP),
-	CAKE(171, 114, 3, Material.CAKE);
+	MILK_BUCKET(0, 250, 0, Material.MILK_BUCKET);
 
 	private int carbs;
 	private int proteins;
 	private int vitamins;
-	private Material material;
+	private Material material = null;
+	private Items item = null;
 
 	Nutrition(int carbs, int proteins, int vitamins, Material material) {
 		this.carbs = carbs;
@@ -57,25 +69,73 @@ public enum Nutrition {
 		this.material = material;
 	}
 
+	@SuppressWarnings("unused") // Will be used in the future for custom food items
+	Nutrition(int carbs, int proteins, int vitamins, Items item) {
+		this.carbs = carbs;
+		this.proteins = proteins;
+		this.vitamins = vitamins;
+		this.item = item;
+	}
+
+	/** Get the carbs for this nutritional item
+	 * @return Carbs for this nutritional item
+	 */
 	public int getCarbs() {
 		return carbs;
 	}
 
+	/** Get the proteins for this nutritional item
+	 * @return Proteins for this nutritional item
+	 */
 	public int getProteins() {
 		return proteins;
 	}
 
+	/** Get the vitamins for this nutritional item
+	 * @return Vitamins for this nutritional item
+	 */
 	public int getVitamins() {
 		return vitamins;
 	}
 
+	/** Get the material of this nutritional item
+	 * @return Material of this nutritional item
+	 */
 	public Material getMaterial() {
 		return material;
 	}
 
+	/** Get the item of this nutritional item
+	 * @return Item of this nutritional item
+	 * @deprecated Currently unused
+	 */
+	@Deprecated
+	public Items getItem() {
+		return item;
+	}
+
+	/** Get from a material
+	 * @param material Material to grab a nutrition from
+	 * @return Nutrition for material (null if nutrition does not exist for this material)
+	 */
 	public static Nutrition getByMaterial(Material material) {
 		for (Nutrition nutrition : values()) {
 			if (nutrition.getMaterial() == material) {
+				return nutrition;
+			}
+		}
+		return null;
+	}
+
+	/** Get from a material
+	 * @param item Item to grab a nutrition from
+	 * @return Nutrition for item (null if nutrition does not exist for this item)
+	 * @deprecated Currently unused
+	 */
+	@Deprecated
+	public static Nutrition getByItem(Items item) {
+		for (Nutrition nutrition : values()) {
+			if (nutrition.getItem() == item) {
 				return nutrition;
 			}
 		}
