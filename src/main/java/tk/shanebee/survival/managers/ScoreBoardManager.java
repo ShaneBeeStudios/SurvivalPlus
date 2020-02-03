fc4a1ae85d7af0c1839157fffedb47bbc9dc2f82
ShaneBee <shanebolenback@me.com>
2020-02-02 16:25:40 -0800

    ScoreBoardManager - changes - Remove old code (we deemed this safe now)

--- src/main/java/tk/shanebee/survival/managers/ScoreBoardManager.java
+++ src/main/java/tk/shanebee/survival/managers/ScoreBoardManager.java
@@ -28,60 +28,6 @@ public class ScoreBoardManager {
      * @param player Player to setup a scoreboard for
      */
     public void setupScoreboard(Player player) {
-        /*
-        Runnable run = new Runnable() {
-            Objective status = stats.registerNewObjective("Status", "dummy", "Status");
-
-            public void run() {
-                status.unregister();
-                status = stats.registerNewObjective("Status", "dummy", "Status");
-
-                PlayerData playerData = plugin.getPlayerManager().getPlayerData(player);
-                if (!player.isOnline()) return;
-
-                GameMode mode = player.getGameMode();
-                if (mode == GameMode.SURVIVAL || mode == GameMode.ADVENTURE) {
-                    status.setDisplaySlot(DisplaySlot.SIDEBAR);
-                    status.setDisplayName("Status");
-
-                    if (playerData.isInfoDisplayed(Info.HUNGER)) {
-                        Score hunger0 = status.getScore(plugin.getPlayerManager().ShowHunger(player).get(0));
-                        hunger0.setScore(10);
-                        Score hunger1 = status.getScore(plugin.getPlayerManager().ShowHunger(player).get(1));
-                        hunger1.setScore(9);
-                        Score hunger2 = status.getScore(plugin.getPlayerManager().ShowHunger(player).get(2));
-                        hunger2.setScore(8);
-                    }
-
-                    if (config.MECHANICS_THIRST_ENABLED && playerData.isInfoDisplayed(Info.THIRST)) {
-                        Score thirst0 = status.getScore(plugin.getPlayerManager().ShowThirst(player).get(0));
-                        thirst0.setScore(7);
-                        Score thirst1 = status.getScore(plugin.getPlayerManager().ShowThirst(player).get(1));
-                        thirst1.setScore(6);
-                        Score thirst2 = status.getScore(plugin.getPlayerManager().ShowThirst(player).get(2));
-                        thirst2.setScore(5);
-                    }
-
-                    if (config.MECHANICS_BED_FATIGUE_ENABLED && playerData.isInfoDisplayed(Info.FATIGUE)) {
-                        Score fatigue = status.getScore(plugin.getPlayerManager().ShowFatigue(player));
-                        fatigue.setScore(4);
-                    }
-
-                    if (config.MECHANICS_FOOD_DIVERSITY_ENABLED && playerData.isInfoDisplayed(Info.NUTRIENTS)) {
-                        Score carbon = status.getScore(plugin.getPlayerManager().ShowNutrients(player).get(0));
-                        carbon.setScore(3);
-                        Score protein = status.getScore(plugin.getPlayerManager().ShowNutrients(player).get(1));
-                        protein.setScore(2);
-                        Score salts = status.getScore(plugin.getPlayerManager().ShowNutrients(player).get(2));
-                        salts.setScore(1);
-                    }
-                }
-
-                Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, this, 10L);
-            }
-        };
-
-         */
         playerBoards.put(player.getUniqueId(), new Healthboard(plugin, player));
     }
 
