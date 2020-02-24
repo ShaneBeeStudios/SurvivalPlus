08a5d7b4c2090e88d8634766e3cad5a1c515df48
ShaneBee <shanebolenback@me.com>
2020-02-23 18:40:01 -0800

    Items - changes - Organize by category

--- src/main/java/tk/shanebee/survival/managers/Items.java
+++ src/main/java/tk/shanebee/survival/managers/Items.java
@@ -9,19 +9,30 @@ import org.bukkit.inventory.ItemStack;
 @SuppressWarnings("DeprecatedIsStillUsed")
 public enum Items {
 
+    // TOOLS
     HATCHET(Material.WOODEN_AXE, 1),
     MATTOCK(Material.WOODEN_PICKAXE, 1),
     SHIV(Material.WOODEN_HOE, 1),
     HAMMER(Material.WOODEN_SWORD, 1),
+    FIRESTRIKER(Material.WOODEN_SHOVEL, 1),
+    GRAPPLING_HOOK(Material.FISHING_ROD, 1),
+    COMPASS(Material.COMPASS, 1),
+    FLINT_SICKLE(Material.WOODEN_HOE, 4),
+    STONE_SICKLE(Material.WOODEN_HOE, 2),
+    IRON_SICKLE(Material.IRON_HOE, 1),
+    DIAMOND_SICKLE(Material.DIAMOND_HOE, 1),
+    MEDIC_KIT(Material.CLOCK, 1),
+    RECURVE_BOW(Material.BOW, 1),
+    RECURVE_CROSSBOW(Material.CROSSBOW, 1),
+
+    // LEGENDARY TOOLS
     VALKYRIES_AXE(Material.DIAMOND_AXE, 1),
     QUARTZ_PICKAXE(Material.DIAMOND_PICKAXE, 1),
     OBSIDIAN_MACE(Material.DIAMOND_SHOVEL, 1),
     ENDER_GIANT_BLADE(Material.DIAMOND_HOE, 2),
     BLAZE_SWORD(Material.DIAMOND_SWORD, 1),
-    WORKBENCH(Material.CRAFTING_TABLE, 0),
-    FIRESTRIKER(Material.WOODEN_SHOVEL, 1),
-    FERMENTED_SKIN(Material.RABBIT_HIDE, 0),
-    MEDIC_KIT(Material.CLOCK, 1),
+
+    // ARMOR
     REINFORCED_LEATHER_BOOTS(Material.CHAINMAIL_BOOTS, 1),
     REINFORCED_LEATHER_TUNIC(Material.CHAINMAIL_CHESTPLATE, 1),
     REINFORCED_LEATHER_TROUSERS(Material.CHAINMAIL_LEGGINGS, 1),
@@ -38,36 +49,34 @@ public enum Items {
     DIAMOND_CHESTPLATE(Material.DIAMOND_CHESTPLATE, 0),
     DIAMOND_HELMET(Material.DIAMOND_HELMET, 0),
     DIAMOND_LEGGINGS(Material.DIAMOND_LEGGINGS, 0),
-    RECURVE_BOW(Material.BOW, 1),
-    RECURVE_CROSSBOW(Material.CROSSBOW, 1),
+    BEEKEEPER_HELMET(Material.LEATHER_HELMET, 10881),
+    BEEKEEPER_CHESTPLATE(Material.LEATHER_CHESTPLATE, 10881),
+    BEEKEEPER_LEGGINGS(Material.LEATHER_LEGGINGS, 10881),
+    BEEKEEPER_BOOTS(Material.LEATHER_BOOTS, 10881),
+
+    // BLOCKS
+    WORKBENCH(Material.CRAFTING_TABLE, 0),
+    CAMPFIRE(Material.CAMPFIRE, 1),
+
+    // MISC
+    FERMENTED_SKIN(Material.RABBIT_HIDE, 0),
+    COFFEE_BEAN(Material.COCOA_BEANS, 1),
+
+    // FOOD
+
+    // DRINKS
     DIRTY_WATER(Material.POTION, 1),
     CLEAN_WATER(Material.POTION, 2),
     PURIFIED_WATER(Material.POTION, 3),
     COFFEE(Material.POTION, 4),
     HOT_MILK(Material.POTION, 5),
     COLD_MILK(Material.POTION, 6),
-    COFFEE_BEAN(Material.COCOA_BEANS, 1),
     @Deprecated // Removed in 3.11.0 - Will remove old water bowl in future
     WATER_BOWL_OLD(Material.BEETROOT_SOUP, 1),
     WATER_BOWL(Material.POTION, 10881),
-    CAMPFIRE(Material.CAMPFIRE, 1),
-
-    FLINT_SICKLE(Material.WOODEN_HOE, 4),
-    STONE_SICKLE(Material.WOODEN_HOE, 2),
-    IRON_SICKLE(Material.IRON_HOE, 1),
-    DIAMOND_SICKLE(Material.DIAMOND_HOE, 1),
-
-    GRAPPLING_HOOK(Material.FISHING_ROD, 1),
-    COMPASS(Material.COMPASS, 1),
-
 
     // TODO Experimental
-    PERSISTENT_TORCH(Material.TORCH, 1),
-
-    BEEKEEPER_HELMET(Material.LEATHER_HELMET, 10881),
-    BEEKEEPER_CHESTPLATE(Material.LEATHER_CHESTPLATE, 10881),
-    BEEKEEPER_LEGGINGS(Material.LEATHER_LEGGINGS, 10881),
-    BEEKEEPER_BOOTS(Material.LEATHER_BOOTS, 10881);
+    PERSISTENT_TORCH(Material.TORCH, 1);
 
     private final Material materialType;
     private final int modelData;
