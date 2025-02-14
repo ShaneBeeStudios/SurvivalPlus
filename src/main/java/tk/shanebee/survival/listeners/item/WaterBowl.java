abba71b56038e3d07b23d7aaa0294a8c80124fb9
ShaneBeee <shanebolenback@me.com>
2025-02-13 17:18:33 -0800

    WaterBowl - cleanup

--- src/main/java/tk/shanebee/survival/listeners/item/WaterBowl.java
+++ src/main/java/tk/shanebee/survival/listeners/item/WaterBowl.java
@@ -1,84 +1,63 @@
 package tk.shanebee.survival.listeners.item;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Keyed;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ItemSpawnEvent;
-import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
-import org.bukkit.inventory.CraftingInventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.Recipe;
+import org.bukkit.scheduler.BukkitScheduler;
 import tk.shanebee.survival.Survival;
 import tk.shanebee.survival.events.WaterBowlFillEvent;
 import tk.shanebee.survival.item.Items;
 
 public class WaterBowl implements Listener {
 
-	private final Survival plugin;
-	private final boolean THIRST_ENABLED;
-	private final boolean CLAY_ENABLED;
+    private final Survival plugin;
+    private final boolean thirstEnabled;
+    private final boolean clayEnabled;
+    private final BukkitScheduler scheduler = Bukkit.getScheduler();
 
-	public WaterBowl(Survival plugin) {
-		this.plugin = plugin;
-		this.THIRST_ENABLED = plugin.getSurvivalConfig().MECHANICS_THIRST_ENABLED;
-		this.CLAY_ENABLED = plugin.getSurvivalConfig().RECIPES_CLAY;
-	}
+    public WaterBowl(Survival plugin) {
+        this.plugin = plugin;
+        this.thirstEnabled = plugin.getSurvivalConfig().MECHANICS_THIRST_ENABLED;
+        this.clayEnabled = plugin.getSurvivalConfig().RECIPES_CLAY;
+    }
 
-	@EventHandler(priority = EventPriority.HIGHEST)
-	private void onConsume(PlayerItemConsumeEvent event) {
-		if (!THIRST_ENABLED) {
-			if (event.isCancelled()) return;
-			if (Items.WATER_BOWL.is(event.getItem())) {
-				event.setCancelled(true);
-			}
-		}
-	}
+    @EventHandler(priority = EventPriority.HIGHEST)
+    private void onConsume(PlayerItemConsumeEvent event) {
+        if (this.thirstEnabled || event.isCancelled()) return;
 
-	@EventHandler
-	private void onDrop(ItemSpawnEvent event) {
-		if (event.isCancelled()) return;
-		if (THIRST_ENABLED || CLAY_ENABLED) {
-			final org.bukkit.entity.Item itemDrop = event.getEntity();
-			if (itemDrop.getItemStack().getType() == Material.BOWL) {
-				final Runnable task = () -> {
-					Location itemLocation = itemDrop.getLocation();
-					if (itemLocation.getBlock().getType() == Material.WATER) {
-						WaterBowlFillEvent bowlFillEvent = new WaterBowlFillEvent(itemDrop.getItemStack());
-						Bukkit.getPluginManager().callEvent(bowlFillEvent);
-						if (bowlFillEvent.isCancelled()) return;
-						int amount = itemDrop.getItemStack().getAmount();
-						itemDrop.remove();
-						for (int i = 0; i < amount; i++) {
-                            itemDrop.getWorld().dropItem(itemLocation, Items.WATER_BOWL.getItemStack());
-                        }
-					}
-				};
-				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, task, 20L);
-			}
-		}
-	}
-
-	// Prevent water bowls turning into glass bottles
-	@EventHandler
-    private void onCraft(PrepareItemCraftEvent event) {
-        Recipe recipe = event.getRecipe();
-        if (recipe instanceof Keyed) {
-            String key = ((Keyed) recipe).getKey().getKey();
-            if (key.equalsIgnoreCase("glass_bottle")) {
-                CraftingInventory inventory = event.getInventory();
-                for (ItemStack itemStack : inventory.getMatrix()) {
-                    if (itemStack != null && Items.WATER_BOWL.is(itemStack)) {
-                        inventory.setResult(null);
-                    }
-                }
-            }
+        if (Items.WATER_BOWL.is(event.getItem())) {
+            event.setCancelled(true);
         }
+    }
 
+    @EventHandler
+    private void onDrop(ItemSpawnEvent event) {
+        if (event.isCancelled()) return;
+        if (!this.thirstEnabled && !this.clayEnabled) return;
+
+        final org.bukkit.entity.Item itemDrop = event.getEntity();
+        if (itemDrop.getItemStack().getType() == Material.BOWL) {
+            this.scheduler.runTaskLater(this.plugin, () -> {
+                Location itemLocation = itemDrop.getLocation();
+                if (itemLocation.getBlock().getType() != Material.WATER) return;
+
+                WaterBowlFillEvent bowlFillEvent = new WaterBowlFillEvent(itemDrop.getItemStack());
+                if (!bowlFillEvent.callEvent()) return;
+
+                World world = itemDrop.getWorld();
+                int amount = itemDrop.getItemStack().getAmount();
+                itemDrop.remove();
+                for (int i = 0; i < amount; i++) {
+                    world.dropItem(itemLocation, Items.WATER_BOWL.getItemStack());
+                }
+            }, 20);
+        }
     }
 
 }
