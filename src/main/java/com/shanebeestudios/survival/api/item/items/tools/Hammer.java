4b9a637edfc30dfcf64a3f1c315a0f41745902d7
ShaneBeee <shanebolenback@me.com>
2025-02-21 16:05:07 -0800

    Hammer upgrades

--- src/main/java/com/shanebeestudios/survival/api/item/items/tools/Hammer.java
+++ src/main/java/com/shanebeestudios/survival/api/item/items/tools/Hammer.java
@@ -2,24 +2,31 @@ package com.shanebeestudios.survival.api.item.items.tools;
 
 import com.shanebeestudios.survival.api.item.Item;
 import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.ItemAttributeModifiers;
 import io.papermc.paper.datacomponent.item.Tool;
 import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.attribute.AttributeModifier.Operation;
+import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ItemType;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.inventory.meta.ItemMeta;
 
 public class Hammer extends Item {
 
     @SuppressWarnings("UnstableApiUsage")
     public Hammer() {
-        ItemStack itemStack = ItemType.WOODEN_AXE.createItemStack();
-        ItemMeta hammerMeta = itemStack.getItemMeta();
-        itemStack.setItemMeta(hammerMeta);
+        ItemStack itemStack = ItemType.STICK.createItemStack();
 
         itemStack.setData(DataComponentTypes.TOOL, Tool.tool()
             .defaultMiningSpeed(0.0001f)
+            .build());
+
+        itemStack.setData(DataComponentTypes.ATTRIBUTE_MODIFIERS, ItemAttributeModifiers.itemAttributes()
+            .addModifier(Attribute.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE, 2.5d, Operation.ADD_NUMBER, EquipmentSlotGroup.MAINHAND))
+            .addModifier(Attribute.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED, -3.0d, Operation.ADD_NUMBER, EquipmentSlotGroup.MAINHAND))
             .build());
 
         setupDefaults("hammer", itemStack);
--- src/main/java/com/shanebeestudios/survival/plugin/listeners/block/BlockPlaceListener.java
+++ src/main/java/com/shanebeestudios/survival/plugin/listeners/block/BlockPlaceListener.java
@@ -1,31 +1,24 @@
 package com.shanebeestudios.survival.plugin.listeners.block;
 
-import com.shanebeestudios.survival.plugin.SurvivalPlugin;
-import com.shanebeestudios.survival.plugin.config.Config;
 import com.shanebeestudios.survival.api.data.Permissions;
 import com.shanebeestudios.survival.api.item.Items;
+import com.shanebeestudios.survival.api.util.BlockTags;
+import com.shanebeestudios.survival.plugin.SurvivalPlugin;
+import com.shanebeestudios.survival.plugin.config.Config;
 import com.shanebeestudios.survival.plugin.managers.MessageManager;
 import com.shanebeestudios.survival.plugin.managers.MessageManager.MessageType;
-import com.shanebeestudios.survival.api.util.BlockTags;
-import com.shanebeestudios.survival.api.util.ItemUtils;
 import org.bukkit.GameMode;
-import org.bukkit.Sound;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.Damageable;
-
-import java.util.Random;
 
 public class BlockPlaceListener implements Listener {
 
     private final Config config;
     private final MessageManager messageManager;
-    private final Random random = new Random();
 
     public BlockPlaceListener(SurvivalPlugin plugin) {
         this.config = plugin.getSurvivalConfig();
@@ -41,33 +34,11 @@ public class BlockPlaceListener implements Listener {
         Player player = event.getPlayer();
         if (Permissions.BYPASS_REQUIRED_TOOLS.has(player)) return;
         if (player.getGameMode() == GameMode.CREATIVE || player.getGameMode() == GameMode.SPECTATOR) return;
+        if (!BlockTags.REQUIRES_HAMMER.isTagged(event.getBlock().getType())) return;
 
-        ItemStack mainTool = player.getInventory().getItemInMainHand();
         ItemStack offTool = player.getInventory().getItemInOffHand();
-        Block block = event.getBlock();
-
-        if (!BlockTags.REQUIRES_HAMMER.isTagged(block.getType())) return;
-
         if (Items.HAMMER.is(offTool)) {
-            int chance_reduceDur = this.random.nextInt(10) + 1;
-            if (chance_reduceDur == 1) {
-                ItemUtils.setDurability(offTool, ItemUtils.getDurability(offTool) + 1);
-            }
-
-            if (ItemUtils.getDurability(offTool) >= offTool.getType().getMaxDurability()) {
-                player.getLocation().getWorld().playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0F, this.random.nextFloat() * 0.4F + 0.8F);
-                player.getInventory().setItemInOffHand(null);
-            }
-        } else if (Items.HAMMER.is(mainTool)) {
-            int chance_reduceDur = this.random.nextInt(10) + 1;
-            if (chance_reduceDur == 1) {
-                ItemUtils.setDurability(mainTool, ((Damageable) mainTool.getItemMeta()).getDamage() + 1);
-            }
-
-            if (ItemUtils.getDurability(mainTool) >= mainTool.getType().getMaxDurability()) {
-                player.getLocation().getWorld().playSound(player.getLocation(), Sound.ENTITY_ITEM_BREAK, 1.0F, this.random.nextFloat() * 0.4F + 0.8F);
-                player.getInventory().setItemInMainHand(null);
-            }
+            offTool.damage(1, player);
         } else {
             event.setCancelled(true);
             player.updateInventory();
